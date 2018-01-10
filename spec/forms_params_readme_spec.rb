7fe53a9746e3e45d5583ba0088f581afbb841754
Steven G. Harms <steven.harms@flatironschool.com>
2018-01-10 10:17:36 -0500

    Add test for ensuring valid form names

--- spec/forms_params_readme_spec.rb
+++ spec/forms_params_readme_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
 describe "App" do
+  describe 'GET /food_form' do
+    it "can fill in the form" do
+      agent = "Carl Jr."
+      food = "Vegan Black-Bean Burger"
+
+      visit "/food_form"
+
+      fill_in("name", with: agent)
+      fill_in("favorite_food", with: food)
+
+      expect(find_field('name').value).to eq agent
+      expect(find_field('favorite_food').value).to eq food
+    end
+  end
+
   describe 'POST /food' do
     it 'responds with a 200' do
       post '/food'
